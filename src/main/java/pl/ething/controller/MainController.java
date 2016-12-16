4e8625fce1b69bc20339de5655a9ea9a01c47f1b
cos <cos@cos.com>
2016-12-16 13:17:06 +0100

    commit test comp.

--- src/main/java/pl/ething/controller/MainController.java
+++ src/main/java/pl/ething/controller/MainController.java
@@ -144,7 +144,6 @@ public class MainController {
         for (Iterator<EthingThing> iterator = things.iterator(); iterator.hasNext();) {
             EthingThing next = iterator.next();
             if (next.getName().contains(text)) {
-                next.setIdhash(mainPage + THING_HTML + next.getIdhash());
                 searchThings.add(next);
             }
         }
